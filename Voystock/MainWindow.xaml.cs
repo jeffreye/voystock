161b54b3a00ef312ba9e81db8144a021727e204d
Jeffrey Ye <lightning.x.revenant@gmail.com>
2015-05-21 15:20:17 +0800

    drag action & ui fixes

--- Voystock/MainWindow.xaml.cs
+++ Voystock/MainWindow.xaml.cs
@@ -24,5 +24,13 @@ namespace Voystock
         {
             InitializeComponent();
         }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
     }
 }
