016142557ae27e28f79cf37ac38e85cd73223e8c
Jeffrey Ye <lightning.x.revenant@gmail.com>
2015-05-24 21:32:34 +0800

    图片归至文件夹,引用mvvmlight

---
+++ Voystock/ViewModel/EvaluationViewModel.cs
@@ -0,0 +1,50 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace Voystock.ViewModel
+{
+	/// <summary>
+	/// This class contains properties that a View can data bind to.
+	/// <para>
+	/// See http://www.galasoft.ch/mvvm
+	/// </para>
+	/// </summary>
+	public class EvaluationViewModel : ViewModelBase
+	{
+		/// <summary>
+		/// Initializes a new instance of the EvaluationPage class.
+		/// </summary>
+		public EvaluationViewModel()
+		{
+		}
+
+		/// <summary>
+		/// The <see cref="SchemeName" /> property's name.
+		/// </summary>
+		public const string SchemeNamePropertyName = "SchemeName";
+
+		private string _schemeName = "MACD(1,2,3)";
+
+		/// <summary>
+		/// Sets and gets the SchemeName property.
+		/// Changes to that property's value raise the PropertyChanged event. 
+		/// </summary>
+		public string SchemeName
+		{
+			get
+			{
+				return _schemeName;
+			}
+
+			set
+			{
+				if (_schemeName == value)
+				{
+					return;
+				}
+
+				_schemeName = value;
+				RaisePropertyChanged(SchemeNamePropertyName);
+			}
+		}
+	}
+}
---
+++ Voystock/ViewModel/RecommendationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace Voystock.ViewModel
+{
+	/// <summary>
+	/// This class contains properties that a View can data bind to.
+	/// <para>
+	/// See http://www.galasoft.ch/mvvm
+	/// </para>
+	/// </summary>
+	public class RecommendationViewModel : ViewModelBase
+	{
+		/// <summary>
+		/// Initializes a new instance of the RecommendationViewModel class.
+		/// </summary>
+		public RecommendationViewModel()
+		{
+		}
+	}
+}
